8a32879fa445d2b10273a60e633191de4063594e
mbarrameda <161363276+mbarrameda@users.noreply.github.com>
2024-04-10 20:23:52 -0400

    fixed comments

--- Pong Game Project/Assets/Ball Movement.cs
+++ Pong Game Project/Assets/Ball Movement.cs
@@ -6,22 +6,23 @@ using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
-    [SerializeField] private float initialSpeed = 10;
-    [SerializeField] private float speedIncrease = 0.25f;
-    [SerializeField] private Text playerScore;
-    [SerializeField] private Text AIScore;
+    [SerializeField] private float initialSpeed = 10; // Initial speed of ball
+    [SerializeField] private float speedIncrease = 0.25f; // Amount by which speed increases after each hit
+    [SerializeField] private Text playerScore; // UI elemenent displaying player's score
+    [SerializeField] private Text AIScore; // UI element displaying AI's score
 
-    private int hitCounter;
-    private Rigidbody2D rb;
+    private int hitCounter; // Counter to keep track of how many times the ball has been hit
+    private Rigidbody2D rb; // Reference to the RigidBody of the ball
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        Invoke("StartBall", 2f);
+        rb = GetComponent<Rigidbody2D>(); 
+        Invoke("StartBall", 2f); // Start ball movement after 2 seconds of delay
     }
 
     private void FixedUpdate()
     {
+        // Just making sure the ball does not exceed the max speed
         rb.velocity = Vector2.ClampMagnitude(rb.velocity, initialSpeed + (speedIncrease * hitCounter));
     }
 
@@ -32,6 +33,7 @@ public class BallMovement : MonoBehaviour
 
     private void ResetBall()
     {
+        // Reset the position, velocity, and hit counter of the ball
         rb.velocity = new Vector2(0, 0);
         transform.position = new Vector2(0, 0);
         hitCounter = 0;
@@ -72,13 +74,16 @@ public class BallMovement : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(transform.position.x > 0)
+        // Check if the ball passes the goal line
+        if (transform.position.x > 0) // Ball passes player's goal
         {
+            // Reset the ball and change player score
             ResetBall();
             playerScore.text = (int.Parse(playerScore.text) + 1).ToString();
         }
         else if(transform.position.x < 0)
         {
+            // Reset ball and change AI score
             ResetBall();
             AIScore.text = (int.Parse(AIScore.text) + 1).ToString();
         }
