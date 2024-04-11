7db3463fcb75231f16389d0593e2bda3ab2865e0
mbarrameda <161363276+mbarrameda@users.noreply.github.com>
2024-04-10 20:02:56 -0400

    fixed ball movement
    fixed ball movement so it doesn't move in a straight line and added active score counter

--- Pong Game Project/Assets/Ball Movement.cs
+++ Pong Game Project/Assets/Ball Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -35,6 +36,52 @@ public class BallMovement : MonoBehaviour
         transform.position = new Vector2(0, 0);
         hitCounter = 0;
         Invoke("StartBall", 2f);
+    }
+
+    private void PlayerBounce(Transform myObject)
+    {
+        hitCounter++;
+
+        Vector2 ballPos = transform.position;
+        Vector2 playerPos = myObject.position;
+
+        float xDirection, yDirection;
+        if (transform.position.x > 0)
+        {
+            xDirection = 1;
+        }
+        else
+        {
+            xDirection = 1;
+        }
+        yDirection = (ballPos.y - playerPos.y) / myObject.GetComponent<Collider2D>().bounds.size.y;
+        if (yDirection == 0)
+        {
+            yDirection = 0.25f;
+        }
+        rb.velocity = new Vector2(xDirection, yDirection) * (initialSpeed + (speedIncrease * hitCounter));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player" || collision.gameObject.name == "AI")
+        {
+            PlayerBounce(collision.transform);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(transform.position.x > 0)
+        {
+            ResetBall();
+            playerScore.text = (int.Parse(playerScore.text) + 1).ToString();
+        }
+        else if(transform.position.x < 0)
+        {
+            ResetBall();
+            AIScore.text = (int.Parse(AIScore.text) + 1).ToString();
+        }
     }
 
 }
